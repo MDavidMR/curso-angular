1904e7a8bb2470186f3d437a86b07213b86d1058
Moises David Morales <mdavid.morales@bestday.com>
2019-06-14 15:24:24 -0500

    23. ¿Qué es una directiva?

--- src/app/empleado/empleado.component.ts
+++ src/app/empleado/empleado.component.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core';
+import { Empleado } from './empleado';
 
 @Component({
     selector: 'empleado',
     templateUrl: './empleado.component.html'
 })
 
-export class EmpleadoComponent{
-    public titulo = 'Empleado';
+export class EmpleadoComponent {
+    public titulo = 'Componente empleados: ';
+    public empleado: Empleado;
+    public trabajadores:Array<Empleado>;
+
+    constructor() {
+        this.empleado = new Empleado('Moises', 31, 'Desarrollador', true);
+        this.trabajadores = [
+            new Empleado('Moises', 31, 'Desarrollador', true),
+            new Empleado('David', 32, 'QA', false),
+            new Empleado('Morales', 33, 'PM', true)
+        ]
+    }
+
+    ngOnInit() {
+        console.log(this.trabajadores);
+
+    }
+
 }
--- src/app/fruta/fruta.component.ts
+++ src/app/fruta/fruta.component.ts
@@ -5,9 +5,50 @@ import { Component } from '@angular/core';
     templateUrl: './fruta.component.html'
 })
 
-export class FrutaComponent{
+export class FrutaComponent {
     public nombre_componente = 'Componente de fruta';
     public listado_frutas = 'Naranja, Manzana, Pera y Sandía';
 
-    public nombre:string = 4;
+    public nombre: string = 'Moises';
+    public edad: number = 55;
+    public mayorDeEdad: boolean = true;
+    public trabajos: Array<string> = ['carpintero', 'albañil', 'fontanero'];
+    public info: Array<any> = [66, 'albañil', 'fontanero'];
+    public comodin: any = 'Comodin'
+
+    constructor() {
+        console.log(this.nombre);
+        console.log(this.info);
+        console.log(this.trabajos)
+
+    }
+
+    ngOnInit() {
+        this.cambiarNombre();
+        this.cambiarEdad(14);
+        // alert(this.nombre + ' ' + this.edad);
+
+        var uno = 8;
+        var dos = 15;
+
+        if (uno === 8) {
+
+            let uno = 3;
+            var dos = 88;
+            console.log("Dentro del IF " + uno);
+        }
+        console.log("Fuera  del IF " + uno);
+
+    }
+
+    cambiarEdad(arg0: number) {
+        this.edad = arg0;
+    }
+    cambiarNombre() {
+        this.nombre = "David";
+    }
+
+    holaMundo() {
+        alert('Hola mundo');
+    }
 }
